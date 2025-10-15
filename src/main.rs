552ed27ac5a19536e12a3d3b3cc6e0f7472e6cff
SRaswan <shauryaraswan@gmail.com>
2025-10-14 18:18:49 -0700

    added main and start

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+// You can add as many files as you want
+
+fn main() {
+    panic!("TODO: implement cobra");
+}
--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -2,17 +2,16 @@ mod infra;
 
 // Your tests go here!
 success_tests! {
-    test_false_val: { file: "false_val", expected: "false" },
     test_input: { file: "input", input: "2", expected: "2" },
 
 }
 
 runtime_error_tests! {
-    test_overflow: { file: "overflow", expected: "overflow" },
+    test_overflow_error: { file: "overflow", expected: "overflow" },
 }
 
 static_error_tests! {
-    test_parse: { file: "parse", expected: "Invalid" },
+    test_parse_error: { file: "parse", input: "2", expected: "Invalid" },
 }
 
 
